51d47bd7f8ebd6e652815a8fd52a37013f691d30
kingsley.ezenwere <kingsley.ezenwere@longbridgetech.com>
2022-09-04 01:32:02 +0100

    upgrade storage

--- terraform/modules/web-app/storage.tf
+++ terraform/modules/web-app/storage.tf
@@ -1,15 +1,44 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
+# resource "aws_s3_bucket" "bucket" {
+#   bucket        = var.bucket_name
+#   force_destroy = true
+#   versioning {
+#     enabled = true
+#   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
+
+
+resource "aws_kms_key" "encrypt_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "tf-backend-state" # REPLACE WITH YOUR BUCKET NAME
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.encrypt_key.arn
+      sse_algorithm     = "aws:kms"
     }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
