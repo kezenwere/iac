3fb94b9fed8113dc918229eb17ee17feaacacd4b
kingsley.ezenwere <kingsley.ezenwere@longbridgetech.com>
2022-09-04 01:48:22 +0100

    upgrade storage

--- terraform/modules/web-app/storage.tf
+++ terraform/modules/web-app/storage.tf
@@ -1,20 +1,3 @@
-# resource "aws_s3_bucket" "bucket" {
-#   bucket        = var.bucket_name
-#   force_destroy = true
-#   versioning {
-#     enabled = true
-#   }
-
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
-
-
 resource "aws_kms_key" "encrypt_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
